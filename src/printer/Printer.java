b1e8350856d1c9159e6338032eb45c7ec8e16ab7
Minisiia <ar_tik@yahoo.com>
2022-11-25 23:26:54 +0200

    Java Essential. 3. Inheritance

---
+++ src/printer/Printer.java
@@ -0,0 +1,37 @@
+package printer;
+
+public class Printer {
+
+    public Printer() {
+    }
+
+    public void print(String value) {
+        System.out.println("value: " + value);
+    }
+
+    public static void main(String[] args) {
+        Printer obj1 = new SubClass1();
+        System.out.println("SubClass1:");
+        obj1.print("Red Message");              // повідомлення похідного класу 1 червоним кольором
+        Printer obj2 = new SubClass2();
+        System.out.println("SubClass2:");       // повідомлення похідного класу 2 бірюзовим кольором
+        obj2.print("Turquoise Message");
+    }
+}
+
+class SubClass1 extends Printer {
+    @Override
+    public void print(String value) {
+        System.out.println((char) 27 + "[31m" + value + (char) 27 + "[38m");
+    }
+}
+
+class SubClass2 extends Printer {
+    @Override
+    public void print(String value) {
+        System.out.println((char) 27 + "[36m" + value + (char) 27 + "[38m");
+    }
+}
+
+
+
